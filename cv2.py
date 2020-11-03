d59a91f4d9dad43b4fb549a51d19de0bf63247e3
jaroon09et <68173312+jaroon09et@users.noreply.github.com>
2020-11-03 11:48:11 +0700

    cv2.py

--- cv2.py
+++ cv2.py
@@ -1,7 +1,9 @@
 from parinya import LINE
 import time
 import cv2
-line= LINE('โทเค็น')
+line= LINE('a4cbbedeca6373e7642fac2399b640fa4008e856
+
+')
 cap= cv2.VideoCapture(0)
   while True:
    _, frame = cap.read()
